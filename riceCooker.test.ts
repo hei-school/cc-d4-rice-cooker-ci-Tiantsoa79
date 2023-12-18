cef0a5ae4cd8d554ce9b834a35bfbd824fdfd543
Tiantsoa79 <hei.tiantsoa.4@gmail.com>
2023-12-18 20:38:14 +0300

    chore: circle ci configuration added

--- riceCooker.test.ts
+++ riceCooker.test.ts
@@ -12,26 +12,26 @@ describe('RiceCooker', () => {
 
   test('cookRice should cook rice', async () => {
     const result = await riceCooker.cookRice();
-    expect(result).toBeTruthy(); // Assuming success returns true
+    expect(result).toBeTruthy(); 
   });
 
   test('keepWarm should keep food warm', async () => {
     const result = await riceCooker.keepWarm();
-    expect(result).toBeTruthy(); // Assuming success returns true
+    expect(result).toBeTruthy(); 
   });
 
   test('steamCook should cook food with steam', async () => {
     const result = await riceCooker.steamCook();
-    expect(result).toBeTruthy(); // Assuming success returns true
+    expect(result).toBeTruthy(); 
   });
 
   test('cookSoup should cook soup', async () => {
     const result = await riceCooker.cookSoup();
-    expect(result).toBeTruthy(); // Assuming success returns true
+    expect(result).toBeTruthy(); 
   });
 
   test('cookDessert should cook dessert', async () => {
     const result = await riceCooker.cookDessert();
-    expect(result).toBeTruthy(); // Assuming success returns true
+    expect(result).toBeTruthy(); 
   });
 });
